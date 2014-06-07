4d2eca07f99332e0fead0e9db3a358f2f1dd2dd7
Jon Cotton <jc@finaltransfer.net>
2014-06-07 13:36:44 -0700

    Use Setuptools and remove incorrect namespacing.

--- armstrong/utils/backends/__init__.py
+++ armstrong/utils/backends/__init__.py
@@ -1,5 +1,1 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
-
-from .base import DID_NOT_HANDLE
-from .base import GenericBackend
+from .base import DID_NOT_HANDLE, GenericBackend
